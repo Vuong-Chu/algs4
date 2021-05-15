7fb4bc740e4639d9440b3e219f2a45496bfec322
Vuong-Chu <minhvuong2992@gmail.com>
2021-05-15 17:57:46 +0100

    Commited

--- src/FastCollinearPoints.java
+++ src/FastCollinearPoints.java
@@ -1,15 +1,13 @@
 import java.util.Arrays;
-import java.util.Comparator;
-import java.util.LinkedList;
-import java.util.List;
 
 public class FastCollinearPoints {
-    private final LineSegment[] segments = new LineSegment[32767];;
+    private final LineSegment[] segments;
     private int index = 0;
     public FastCollinearPoints(Point[] points){
         if(points==null){
             throw new IllegalArgumentException();
         }
+        segments = new LineSegment[points.length+1];
         for(Point p : points){
             if(p==null) {
                 throw new IllegalArgumentException();
@@ -22,27 +20,32 @@ public class FastCollinearPoints {
                 }
             }
         }
-        Point[] auxPoints = Arrays.copyOf(points,points.length);
+        Point[] auxPoints = points.clone();
         if(auxPoints.length >= 4){
             for(int i=0; i<auxPoints.length; i++) {
-            Point[] tempPoints = new Point[auxPoints.length - i];
-            for (int k = 0; k < auxPoints.length - i; k++) {
-                tempPoints[k] = auxPoints[k + i];
-            }
-            Point temp = auxPoints[i];
-            Arrays.sort(tempPoints, temp.slopeOrder());
-            int count = 1;
-            for (int j = 0; j < tempPoints.length - 1; j++) {
-                if (temp.slopeTo(tempPoints[j]) == temp.slopeTo(tempPoints[j + 1])) {
-                    count++;
-                } else {
-                    if (count >= 3) {
-                        segments[index++] = new LineSegment(temp, tempPoints[j]);
+                Arrays.sort(auxPoints);
+                Point temp = auxPoints[i];
+                Arrays.sort(auxPoints, temp.slopeOrder());
+                int count=1;
+                boolean order = true;
+                for (int j = 1; j < auxPoints.length; j++) {
+                    if(temp.slopeTo(auxPoints[j-1])==temp.slopeTo(auxPoints[j])){
+                        if(!(temp.compareTo(auxPoints[j-1])<0 && temp.compareTo(auxPoints[j])<0)){
+                            order = false;
+                        }
+                        count++;
+                        if(count>=3 && j==auxPoints.length-1 && order){
+                            segments[index++] = new LineSegment(temp,auxPoints[j]);
+                        }
+                    }else{
+                        if(count>=3 && order){
+                            segments[index++] = new LineSegment(temp,auxPoints[j-1]);
+                        }
+                        count=1;
+                        order=true;
                     }
-                    count = 1;
                 }
             }
-        }
         }
     }
     public int numberOfSegments(){
@@ -52,33 +55,22 @@ public class FastCollinearPoints {
         return Arrays.copyOf(segments,index);
     }
 
-    public static void main(String[] args){
-        FastCollinearPoints fastCollinearPoints1 = new FastCollinearPoints(new Point[]
-                {       new Point(10000,      0 ),
-                        new Point(0,  10000 ),
-                        new Point(3000,   7000 ),
-                        new Point(7000,   3000 ),
-                        new Point(20000,  21000),
-                        new Point(3000,   4000),
-                        new Point(14000,  15000),
-                        new Point(6000,   7000)
-                });
-        System.out.println(fastCollinearPoints1.numberOfSegments());
-        for(LineSegment ls: fastCollinearPoints1.segments()){
-            System.out.println(ls.toString());
-        }
-
-        FastCollinearPoints fastCollinearPoints2 = new FastCollinearPoints(new Point[]
-                {       new Point(19000,  10000 ),
-                        new Point(18000,  10000),
-                        new Point(32000,  10000  ),
-                        new Point(21000,  10000 ),
-                        new Point( 1234,   5678 ),
-                        new Point(14000,  10000)
-                });
-        System.out.println(fastCollinearPoints2.numberOfSegments());
-        for(LineSegment ls: fastCollinearPoints2.segments()){
-            System.out.println(ls.toString());
-        }
-    }
+//    public static void main(String[] args) throws FileNotFoundException {
+//
+//        // read the n points from a file
+//        In in = new In(new File("src/input4000.txt"));
+//        int n = in.readInt();
+//        Point[] points = new Point[n];
+//        for (int i = 0; i < n; i++) {
+//            int x = in.readInt();
+//            int y = in.readInt();
+//            points[i] = new Point(x, y);
+//        }
+//
+//        // print and draw the line segments
+//        FastCollinearPoints collinear = new FastCollinearPoints(points);
+//        for(LineSegment ls: collinear.segments()){
+//            System.out.println(ls.toString());
+//        }
+//    }
 }
--- src/SampleClient.java
+++ src/SampleClient.java
@@ -1,12 +1,16 @@
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 
 public class SampleClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
         // read the n points from a file
-        In in = new In(args[0]);
+        In in = new In(new File("src/input4000.txt"));
         int n = in.readInt();
         Point[] points = new Point[n];
         for (int i = 0; i < n; i++) {
